8d2cab7c5f646ad3179ddea6d2b9c25d6f1b429e
tomoki-akasaka <t-akasaka@ist.osaka-u.ac.jp>
2022-08-05 18:14:51 +0900

    update

--- Assets/tomoki_akasaka/Scrpts/Special.cs
+++ Assets/tomoki_akasaka/Scrpts/Special.cs
@@ -7,10 +7,16 @@ public class Special : MonoBehaviour
 {
     public GameObject chargeBullet;
     public bool skill1;
+    public float chargeTime;
+    AudioSource audioSource;
+
+    public AudioClip chargeAudio;
+    public AudioClip chargeShotAudio;
     // Start is called before the first frame update
     void Start()
     {
         skill1 = true;
+        audioSource = transform.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,9 +33,19 @@ public class Special : MonoBehaviour
         }
         else
         {
+        Debug.Log("aa");
         Vector3 pos = obj.transform.Find("bulletPosition").gameObject.transform.position;
-        Instantiate(chargeBullet, pos, obj.transform.rotation).transform.SetParent(obj.transform);
+        GameObject bullet = Instantiate(chargeBullet, pos, obj.transform.rotation);
+        audioSource.PlayOneShot(chargeAudio);
+        bullet.transform.SetParent(obj.transform);
         skill1 = false;
+        yield return new WaitForSeconds(chargeTime);
+
+        var shotForward = (pos -obj.transform.Find("muzzleflash_rifle").gameObject.transform.position).normalized;
+        bullet.GetComponent<BulletController>().Shoot(shotForward);
+        audioSource.Stop();
+        audioSource.PlayOneShot(chargeShotAudio);
+
         }
 
     }
