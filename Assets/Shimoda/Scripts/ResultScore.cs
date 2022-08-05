4429f7f52eab8fa67f83cfeee5bc9a470f5c5f5f
kantashimoda <shkanta79@gmail.com>
2022-08-06 05:36:29 +0900

    リザルト実装

---
+++ Assets/Shimoda/Scripts/ResultScore.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScore : MonoBehaviour
+{
+    private Text resultScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultScore = this.gameObject.GetComponent<Text>();
+        resultScore.text = GameObject.Find("ScoreManager").GetComponent<ScoreManager>().EndScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
